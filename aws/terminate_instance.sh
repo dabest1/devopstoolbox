931e11903f4acb2c0eb4991060b3b0bef5c9a198
dabest1 <dabest1@users.noreply.github.com>
2016-04-23 13:42:01 -0700

    Add terminate_instance.sh.

---
+++ aws/terminate_instance.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+# Purpose:
+#     Terminate AWS instance and delete attached volumes.
+# Usage:
+#     Run script with no options to get usage.
+
+version=1.0.0
+
+name="$1"
+log='terminate_instance.log'
+profile="$AWS_PROFILE"
+
+set -o pipefail
+failures=0
+if [[ -z $name || -z $profile ]]; then
+    echo 'Usage:'
+    echo '    export AWS_PROFILE=profile'
+    echo '    script.sh name|instance_id'
+    exit 1
+fi
+
+echo >> $log
+echo >> $log
+date +'%F %T %z' >> $log
+echo "profile: $profile" | tee -a $log
+if echo "$name" | grep -q 'i-'; then
+    instance_id="$name"
+else
+    echo "name: $name" | tee -a $log
+    instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+fi
+echo "instance_id: $instance_id" | tee -a $log
+if [[ -z $instance_id ]]; then
+    exit 1
+fi
+
+aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
+aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> $log
+
+aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a $log
+
+volume_ids=$(aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" "Name=attachment.delete-on-termination, Values=false" --query 'Volumes[*].[VolumeId]' --output text)
+
+echo -n 'Are you sure that you want this instance terminated? y/n: '
+read yn
+if [[ $yn == y ]]; then
+    aws --profile "$profile" ec2 terminate-instances --instance-ids "$instance_id" --output table | tee -a $log
+else
+    echo 'Aborted!' | tee -a $log
+    exit 1
+fi
+
+if [[ -z $volume_ids ]]; then
+    exit 0
+fi
+echo "volume_ids:" $volume_ids | tee -a $log
+echo -n 'Are you sure that you want these volumes deleted? y/n: '
+read yn
+if [[ $yn == y ]]; then
+    for volume_id in $volume_ids; do
+        echo "Deleting volume_id: $volume_id" | tee -a $log
+        aws --profile "$profile" ec2 delete-volume --volume-id "$volume_id" &> /dev/null
+        return_code=$?
+        while [[ $return_code -eq 255 ]]; do # Wait until the instance is terminated to delete the volume.
+            echo -n '.'
+            sleep 10
+            aws --profile "$profile" ec2 delete-volume --volume-id "$volume_id" &> /dev/null
+            return_code=$?
+        done
+        if [[ $return_code -ne 0 ]]; then
+            echo "Error: Could not delete volume." | tee -a $log
+            echo "return_code: $return_code" | tee -a $log
+            failures+=1
+        else
+            echo 'done' | tee -a $log
+        fi
+    done
+else
+    echo 'Aborted!' | tee -a $log
+    exit 1
+fi
+
+exit "$failures"

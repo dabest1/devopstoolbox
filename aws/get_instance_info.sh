2cebf5ab25aa7cea76ccfbfc2d2abab43e26a4d0
dabest1 <dabest1@users.noreply.github.com>
2016-04-23 13:30:53 -0700

    Added ability to display info about multiple instances. Some other changes.

--- aws/get_instance_info.sh
+++ aws/get_instance_info.sh
@@ -1,35 +1,40 @@
 #!/bin/bash
 
 # Purpose:
-#     Prints essential information about the host supplied.
+#     Prints essential information about the instance name supplied.
+#     Information about multiple instances can be displayed if partial name 
+#     with wildcard is supplied.
 # Usage:
-#     Run with no options to get usage.
+#     Run script with no options to get usage.
 
-version=1.0.1
+version=1.0.2
 
-host="$1"
+name="$1"
 log='get_instance_info.log'
 profile="$AWS_PROFILE"
 
-if [[ -z $host || -z $profile ]]; then
+if [[ -z $name || -z $profile ]]; then
     echo 'Usage:'
     echo '    export AWS_PROFILE=profile'
-    echo '    script.sh hostname'
+    echo '    script.sh name'
+    echo '    or'
+    echo "    script.sh 'partial_name*'"
     exit 1
 fi
 
-echo "profile: $profile"
-echo "host: $host"
-
-instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$host" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+echo >> $log
+echo >> $log
+echo "profile: $profile" | tee -a $log
+echo "name: $name" | tee -a $log
+instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
 return_code=$?
 if [[ $return_code -ne 0 ]]; then
     exit 1
 fi
-echo "instance_id: $instance_id"
+echo "instance_id(s):" $instance_ids | tee -a $log
 
-aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table | tee -a $log
-
-aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a $log
-
-aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" >> $log
+for instance_id in $instance_ids; do
+    echo | tee -a $log
+    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table | tee -a $log
+    aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a $log
+done

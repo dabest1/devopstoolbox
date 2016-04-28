6195bc1341ee185d22bbf5f9a9f1e90710f4e708
Dimitriy Alekseyev <dimitriy.alekseyev@glu.com>
2016-04-27 19:06:42 -0700

    Added ability to list instances by id.

--- aws/list_instance.sh
+++ aws/list_instance.sh
@@ -7,10 +7,11 @@
 # Usage:
 #     Run script with -h option to get usage.
 
-version=1.0.2
+version=1.0.3
 
 name="$1"
 profile="$AWS_PROFILE"
+output='text'
 
 if [[ -z $profile || $1 == '-h' ]]; then
     echo 'Usage:'
@@ -25,14 +26,15 @@ if [[ -z $1 ]]; then
 fi
 
 echo "profile: $profile"
-echo "name: $name"
-instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
-return_code=$?
-if [[ $return_code -ne 0 ]]; then
-    exit 1
+if echo "$name" | grep -q 'i-'; then
+    instance_ids="$name"
+else
+    echo "name: $name"
+    instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
 fi
 echo "instance_ids:" $instance_ids
 if [[ -z $instance_ids ]]; then
     exit 1
 fi
-aws --profile "$profile" ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
+
+aws --profile "$profile" ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output "$output"

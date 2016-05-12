cf4a83c5b83c700cfb197e6faf60a96887ef0308
dabest1 <dabest1@users.noreply.github.com>
2016-05-11 23:55:08 -0700

    Display instance information if volume is attached. Now waiting for detach to complete before moving on to next step.

--- aws/delete_volume.sh
+++ aws/delete_volume.sh
@@ -5,7 +5,7 @@
 # Usage:
 #     Run script with no options to get usage.
 
-version=1.0.2
+version=1.0.3
 
 volume_id="$1"
 log='delete_volume.log'
@@ -21,16 +21,23 @@ fi
 echo >> $log
 echo >> $log
 date +'%F %T %z' >> $log
+echo "profile: $profile" | tee -a $log
 echo "volume_id: $volume_id" | tee -a $log
+echo | tee -a $log
 
 aws --profile "$profile" ec2 describe-volumes --volume-ids "$volume_id" --output table | tee -a $log
 rc=$?
 if [[ $rc -ne 0 ]]; then
-    echo 'Error getting volume information.'
+    echo 'Error getting volume information.' | tee -a $log
     exit 1
 fi
+result=$(aws --profile "$profile" ec2 describe-volumes --volume-ids "$volume_id" --output json)
+instance_id=$(echo "$result" | awk -F'"' '/"InstanceId":/{print $4}')
+if [[ ! -z $instance_id ]]; then
+    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table | tee -a $log
+fi
 
-echo -n 'Are you sure that you want this volume deleted? y/n: '
+echo -n 'Are you sure that you want this volume deleted? y/n: ' | tee -a $log
 read yn
 if [[ $yn == y ]]; then
     result=$(aws --profile "$profile" ec2 describe-volumes --volume-ids "$volume_id" --query 'Volumes[*].{InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,Device:Attachments[0].Device}')
@@ -39,24 +46,33 @@ if [[ $yn == y ]]; then
     attach_state=$(echo "$result" | awk -F'"' '/"State":/{print $4}')
     if [[ $attach_state == 'attached' || $attach_state == 'busy' ]]; then
         name=$(aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0]]' --output text)
-        echo 'Unmount volume...'
-        ssh "$name" "cmd=\$(cat /etc/fstab | grep '/dev/xvd${device: -1}' | awk '{print \$1}' | xargs echo sudo umount); echo \$cmd; eval \$cmd"
+        echo 'Unmount volume...' | tee -a $log
+        ssh "$name" "cmd=\$(cat /etc/fstab | grep '/dev/xvd${device:(-1)}' | awk '{print \$1}' | xargs echo sudo umount); echo \$cmd; eval \$cmd"
         rc=$?
         if [[ $rc -ne 0 ]]; then
-            echo 'Error unmounting volume.'
-            exit 1
+            echo 'Warning: Could not unmount volume.' | tee -a $log
         fi
-        echo 'Detach volume...'
-        aws --profile "$profile" ec2 detach-volume --volume-id "$volume_id"
+        echo 'Done.'
+
+        echo 'Detach volume...' | tee -a $log
+        aws --profile "$profile" ec2 detach-volume --volume-id "$volume_id" | tee -a $log
+        state=""
+        while [[ $state != 'available' ]]; do
+            result=$(aws --profile "$profile" ec2 describe-volumes --volume-ids "$volume_id" --query 'Volumes[*].{State:State}' --output json)
+            state=$(echo "$result" | awk -F'"' '/"State":/{print $4}')
+            echo -n "."
+            sleep 1
+        done
+        echo 'Done.' | tee -a $log
     fi
-    echo "Deleting volume_id: $volume_id" | tee -a $log
+    echo "Delete volume..." | tee -a $log
     aws --profile "$profile" ec2 delete-volume --volume-id "$volume_id" | tee -a $log
     rc=$?
     if [[ $rc -ne 0 ]]; then
-        echo 'Error deleting volume.'
+        echo 'Error deleting volume.' | tee -a $log
         exit 1
     fi
-    echo 'Done.'
+    echo 'Done.' | tee -a $log
 else
     echo 'Aborted!' | tee -a $log
     exit 1

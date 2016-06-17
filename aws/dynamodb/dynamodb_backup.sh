23c697c75663239a3dee70ac070dd66dcfce2c5a
Dimitriy Alekseyev <dimitriy.alekseyev@glu.com>
2016-06-17 13:18:11 -0700

    Modified script to use configuration file, instead of storing configuration within the script.

--- aws/dynamodb/dynamodb_backup.sh
+++ aws/dynamodb/dynamodb_backup.sh
@@ -2,17 +2,24 @@
 
 # Purpose:
 #     Backup AWS DynamoDB tables. Script wrapper for dynamodump.py.
+# Usage:
+#     Run script with --help option to get usage.
 
-version=1.0.0
+version="1.0.1"
 
-dynamodump="python /root/git/dynamodump/dynamodump.py"
-region="us-east-1"
-accessKey=""
-secretKey=""
+set -o pipefail
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_name="$(basename "$0")"
+config_path="$script_dir/${script_name/.sh/.cfg}"
 
-tables="table1
-table2
-table3"
+# Load configuration settings.
+source "$config_path"
+
+if [[ $1 == '--help' ]]; then
+    echo 'Usage:'
+    echo "    $script_name"
+    exit 1
+fi
 
 for table in $tables; do
     echo "Table: $table"

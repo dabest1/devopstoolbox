a44208e5d4a47c46b26c35f7d9dcaba1ee6a2043
Dimitriy Alekseyev <dimitriy.alekseyev@glu.com>
2016-06-23 18:14:38 -0700

    Added read capacity settings and made other improvements.

--- aws/dynamodb/dynamodb_backup.sh
+++ aws/dynamodb/dynamodb_backup.sh
@@ -5,7 +5,7 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.2"
+version="1.0.3"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -30,31 +30,27 @@ echo "* Time started: $start_time"
 echo "**************************************************"
 echo
 echo "Hostname: $HOSTNAME"
-echo "Backup TS: $bkup_ts"
+echo "bkup_ts: $bkup_ts"
+echo "script: $script_dir/$script_name"
+echo "dynamodump: $dynamodump"
+echo "readCapacity: $readCapacity"
+echo "region: $region"
+echo "tables: $tables"
 echo
 
 for table in $tables; do
     date -u +'TS: %Y%m%dT%H%M%SZ'
     echo "Table: $table"
-    $dynamodump -r $region --accessKey $accessKey --secretKey $secretKey -m backup -s $table
-    #$dynamodump -r $region --accessKey $accessKey --secretKey $secretKey -m restore -s $table -d new_table_name 
+    $dynamodump -r "$region" --accessKey "$accessKey" --secretKey "$secretKey" -m backup --readCapacity "$readCapacity" -s "$table"
     echo
 done
 
 date -u +'TS: %Y%m%dT%H%M%SZ'
-mv dump "$bkup_ts"
-
-cat > "$bkup_ts/backup_info.txt" <<HEREDOCUMENT
-Backup of AWS DynamoDB tables.
-
-bkup_ts: $bkup_ts
-script: $script_dir/$script_name
-bkup_method: $dynamodump
-HEREDOCUMENT
-
+mv -v dump "$bkup_ts"
 echo "Size of backup:"
 du -s "$bkup_ts/"*
 echo
+
 echo "**************************************************"
 echo "* Time finished: $(date -u +'%F %T %Z')"
 echo "**************************************************"

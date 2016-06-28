e7a62b957a3a37961e01e6ae988780cf13b86554
dabest1 <dabest1@users.noreply.github.com>
2016-06-27 18:18:13 -0700

    Added different backup step for config server versus replica set member. Also other changes.

--- mongodb/backup/mongo_backup_all_dbs.sh
+++ mongodb/backup/mongo_backup_all_dbs.sh
@@ -10,14 +10,12 @@
 #	To restore the backup:
 #	find "backup_path" -name "*.bson.gz" -exec gunzip '{}' \;
 #	mongorestore --oplogReplay --dir "backup_path"
-#
-# Source:
-#	https://github.com/dabest1/mongodb/blob/master/backup/mongo_backup_all_dbs.sh
 ################################################################################
 
 # Version.
-version="1.1.0"
+version="1.1.1"
 
+start_time="$(date -u +'%F %T %Z')"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 config_path="$script_dir/${script_name/.sh/.cfg}"
@@ -26,23 +24,22 @@ config_path="$script_dir/${script_name/.sh/.cfg}"
 source "$config_path"
 
 if [[ -z $bkup_dir ]]; then
-	echo "Error: Some variables were not provided in configuration file." >&2
-	exit 1
+    echo "Error: Some variables were not provided in configuration file." >&2
+    exit 1
 fi
 
 # Redirect stderr into error log, stdout and stderr into log and terminal.
-log_err="$bkup_dir/error.log"
-log_mail="$bkup_dir/mail.log"
-rm "$log_err" "$log_mail" 2> /dev/null
-exec 1> >(tee -ia "$log_mail") 2> >(tee -ia "$log_mail" >&2) 2> >(tee -ia "$log_err" >&2)
+log="${log:-/tmp/backup.log}"
+log_err="${log_err:-/tmp/backup.err}"
+rm "$log" "$log_err" 2> /dev/null
+exec 1> >(tee -ia "$log") 2> >(tee -ia "$log" >&2) 2> >(tee -ia "$log_err" >&2)
 
-start_time="$(date -u +'%F %T %Z')"
 bkup_date="$(date -d "$start_time" +'%Y%m%dT%H%M%SZ')"
 bkup_dow="$(date -d "$start_time" +'%w')"
 if [[ -z "$user" ]]; then
-        mongo_option=""
+    mongo_option=""
 else
-        mongo_option="-u $user -p $pass"
+    mongo_option="-u $user -p $pass"
 fi
 
 echo "**************************************************"
@@ -51,6 +48,8 @@ echo "* Time started: $start_time"
 echo "**************************************************"
 echo
 echo "Hostname: $HOSTNAME"
+echo "MongoDB version:"
+mongod --version
 echo
 
 # Decide on what type of backup to perform.
@@ -84,7 +83,22 @@ echo "Backup type: $bkup_type"
 echo "Number of backups to retain for this type: $num_bkups"
 echo
 
+echo "Backup will be created in:"
+echo "$bkup_dir/$bkup_date.$bkup_type"
+echo
+mkdir "$bkup_dir/$bkup_date.$bkup_type"
+# Move logs into dated backup directory.
+mv "$log" "$bkup_dir/$bkup_date.$bkup_type/"
+mv "$log_err" "$bkup_dir/$bkup_date.$bkup_type/"
+log="$bkup_dir/$bkup_date.$bkup_type/$(basename "$log")"
+log_err="$bkup_dir/$bkup_date.$bkup_type/$(basename "$log_err")"
+
 # Purge old backups.
+echo "Disk space before purge:"
+df -h "$bkup_dir"
+echo
+
+echo "Purge old backups."
 list_of_bkups="$(find "$bkup_dir" -name "*.$bkup_type" | sort)"
 if [[ ! -z "$list_of_bkups" ]]; then
 	while [[ "$(echo "$list_of_bkups" | wc -l)" -ge $num_bkups ]]
@@ -98,21 +112,28 @@ if [[ ! -z "$list_of_bkups" ]]; then
 fi
 
 # Perform backup.
-echo "Backup will be created in:"
-echo "$bkup_dir/$bkup_date.$bkup_type"
-echo
 echo "Disk space before backup:"
 df -h "$bkup_dir"
 echo
-mkdir "$bkup_dir/$bkup_date.$bkup_type"
-echo "Backing up all dbs except local with --oplog option."
+
 date -u +'start:  %F %T %Z'
-"$mongodump" $mongo_option -o "$bkup_dir/$bkup_date.$bkup_type" --authenticationDatabase admin --oplog 2> "$bkup_dir/$bkup_date.$bkup_type/mongodump.log"
-rc=$?
+if echo "$HOSTNAME" | grep -q 'cfgdb'; then
+    # Config server
+    echo "Backing up config server."
+    "$mongodump" $mongo_option -o "$bkup_dir/$bkup_date.$bkup_type" --authenticationDatabase admin 2> "$bkup_dir/$bkup_date.$bkup_type/mongodump.log"
+    rc=$?
+else
+    # Replica set member
+    echo "Backing up all dbs except local with --oplog option."
+    "$mongodump" $mongo_option -o "$bkup_dir/$bkup_date.$bkup_type" --authenticationDatabase admin --oplog 2> "$bkup_dir/$bkup_date.$bkup_type/mongodump.log"
+    rc=$?
+fi
 if [[ $rc -ne 0 ]]; then
 	cat "$bkup_dir/$bkup_date.$bkup_type/mongodump.log" >&2
 fi
 date -u +'finish: %F %T %Z'
+
+echo
 echo "Total disk usage:"
 du -sb "$bkup_dir/$bkup_date.$bkup_type"
 echo "Disk space after backup:"
@@ -124,30 +145,38 @@ echo "Compress backup."
 date -u +'start:  %F %T %Z'
 find "$bkup_dir/$bkup_date.$bkup_type" -name "*.bson" -exec gzip '{}' \;
 date -u +'finish: %F %T %Z'
+echo
 echo "Total compressed disk usage:"
 du -sb "$bkup_dir/$bkup_date.$bkup_type"
 echo "Disk space after compression:"
 df -h "$bkup_dir"
 echo
 
+if [[ ! -z $post_backup ]]; then
+    cd "$script_dir"
+    echo "Post backup process."
+    echo "Command:"
+    eval echo "$post_backup"
+    date -u +'start:  %F %T %Z'
+    eval "$post_backup"
+    post_backup_rc=$?
+    date -u +'finish: %F %T %Z'
+    echo
+fi
+
 echo "**************************************************"
-echo "* Backup MongoDB Database"
 echo "* Time finished: $(date -u +'%F %T %Z')"
 echo "**************************************************"
 
 # Send email.
-if [[ -s "$log_err" ]]; then
-	if [[ ! -z "$mail_on_error" ]]; then
-        	mail -s "Error - MongoDB Backup $HOSTNAME" "$mail_on_error" < "$log_mail"
-	fi
-	mv "$log_mail" "$bkup_dir/$bkup_date.$bkup_type/"
-	mv "$log_err" "$bkup_dir/$bkup_date.$bkup_type/"
-	exit 1
+if [[ -s "$log_err" || $post_backup_rc -gt 0 ]]; then
+    if [[ ! -z "$mail_on_error" ]]; then
+        mail -s "Error - MongoDB Backup $HOSTNAME" "$mail_on_error" < "$log"
+    fi
+    exit 1
 else
-	if [[ ! -z "$mail_on_success" ]]; then
-        	mail -s "Success - MongoDB Backup $HOSTNAME" "$mail_on_success" < "$log_mail"
-	fi
-	mv "$log_mail" "$bkup_dir/$bkup_date.$bkup_type/"
-	mv "$log_err" "$bkup_dir/$bkup_date.$bkup_type/"
-	exit 0
+    if [[ ! -z "$mail_on_success" ]]; then
+       	mail -s "Success - MongoDB Backup $HOSTNAME" "$mail_on_success" < "$log"
+    fi
+    exit 0
 fi

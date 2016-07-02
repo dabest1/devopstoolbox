689a4eb5eb80209010c16701b6b51463070d85ac
Dimitriy Alekseyev <dimitriy.alekseyev@glu.com>
2016-07-01 21:11:43 -0700

    Fixed formatting.

--- mongodb/backup/mongo_backup_all_dbs.sh
+++ mongodb/backup/mongo_backup_all_dbs.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 ################################################################################
 # Purpose:
-#	Backup all MongoDB databases using mongodump (local db is excluded.).
-#	--oplog option is used, so that backup will reflect a single moment in 
-#	time.
-#	Compress backup.
-#	Send email upon completion.
+#     Backup all MongoDB databases using mongodump (local db is excluded.).
+#     --oplog option is used, so that backup will reflect a single moment in 
+#     time.
+#     Compress backup.
+#     Send email upon completion.
 #
-#	To restore the backup:
-#	find "backup_path" -name "*.bson.gz" -exec gunzip '{}' \;
-#	mongorestore --oplogReplay --dir "backup_path"
+#     To restore the backup:
+#     find "backup_path" -name "*.bson.gz" -exec gunzip '{}' \;
+#     mongorestore --oplogReplay --dir "backup_path"
 ################################################################################
 
 # Version.
-version="1.1.2"
+version="1.1.3"
 
 start_time="$(date -u +'%F %T %Z')"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -54,32 +54,32 @@ echo
 
 # Decide on what type of backup to perform.
 if [[ -z "$bkup_type" ]]; then
-	# Check if daily or weekly backup should be run.
-	if [[ $bkup_dow -eq $weekly_bkup_dow ]]; then
-		# Check if it is time to run monthly or yearly backup.
-		bkup_y="$(date -d "$start_time" +'%Y')"
-		yearly_bkup_exists="$(find "$bkup_dir" -name "*.yearly" | awk -F'/' '{print $NF}' | grep "^$bkup_y")"
-		bkup_ym="$(date -d "$start_time" +'%Y%m')"
-		monthly_bkup_exists="$(find "$bkup_dir" -name "*.monthly" | awk -F'/' '{print $NF}' | grep "^$bkup_ym")"
-		bkup_yw="$(date -d "$start_time" +'%Y%U')"
-		weekly_bkup_exists="$(find "$bkup_dir" -name "*.weekly" | awk -F'/' '{print $NF}' | awk -FT '{print $1}' | xargs -i date -d "{}" +'%Y%U' | grep "^$bkup_yw")"
-		if [[ -z "$yearly_bkup_exists" && $num_yearly_bkups -ne 0 ]]; then
-			bkup_type="yearly"
-			num_bkups=$num_yearly_bkups
-		elif [[ -z "$monthly_bkup_exists" && $num_monthly_bkups -ne 0 ]]; then
-			bkup_type="monthly"
-			num_bkups=$num_monthly_bkups
-		elif [[ -z "$weekly_bkup_exists" && $num_weekly_bkups -ne 0 ]]; then
-			bkup_type="weekly"
-			num_bkups=$num_weekly_bkups
-		else
-			bkup_type="daily"
-			num_bkups=$num_daily_bkups
-		fi
-	else
-		bkup_type="daily"
-		num_bkups=$num_daily_bkups
-	fi
+    # Check if daily or weekly backup should be run.
+    if [[ $bkup_dow -eq $weekly_bkup_dow ]]; then
+        # Check if it is time to run monthly or yearly backup.
+        bkup_y="$(date -d "$start_time" +'%Y')"
+        yearly_bkup_exists="$(find "$bkup_dir" -name "*.yearly" | awk -F'/' '{print $NF}' | grep "^$bkup_y")"
+        bkup_ym="$(date -d "$start_time" +'%Y%m')"
+        monthly_bkup_exists="$(find "$bkup_dir" -name "*.monthly" | awk -F'/' '{print $NF}' | grep "^$bkup_ym")"
+        bkup_yw="$(date -d "$start_time" +'%Y%U')"
+        weekly_bkup_exists="$(find "$bkup_dir" -name "*.weekly" | awk -F'/' '{print $NF}' | awk -FT '{print $1}' | xargs -i date -d "{}" +'%Y%U' | grep "^$bkup_yw")"
+        if [[ -z "$yearly_bkup_exists" && $num_yearly_bkups -ne 0 ]]; then
+            bkup_type="yearly"
+            num_bkups=$num_yearly_bkups
+        elif [[ -z "$monthly_bkup_exists" && $num_monthly_bkups -ne 0 ]]; then
+            bkup_type="monthly"
+            num_bkups=$num_monthly_bkups
+        elif [[ -z "$weekly_bkup_exists" && $num_weekly_bkups -ne 0 ]]; then
+            bkup_type="weekly"
+            num_bkups=$num_weekly_bkups
+        else
+            bkup_type="daily"
+            num_bkups=$num_daily_bkups
+        fi
+    else
+        bkup_type="daily"
+        num_bkups=$num_daily_bkups
+    fi
 fi
 echo "Backup type: $bkup_type"
 echo "Number of backups to retain for this type: $num_bkups"
@@ -103,14 +103,13 @@ echo
 echo "Purge old backups."
 list_of_bkups="$(find "$bkup_dir" -name "*.$bkup_type" | sort)"
 if [[ ! -z "$list_of_bkups" ]]; then
-	while [[ "$(echo "$list_of_bkups" | wc -l)" -ge $num_bkups ]]
-	do
-		old_bkup="$(echo "$list_of_bkups" | head -1)"
-		echo "Deleting old backup: $old_bkup"
-		rm -r "$old_bkup"
-		list_of_bkups="$(find "$bkup_dir" -name "*.$bkup_type" | sort)"
-	done
-	echo
+    while [[ "$(echo "$list_of_bkups" | wc -l)" -ge $num_bkups ]]; do
+        old_bkup="$(echo "$list_of_bkups" | head -1)"
+        echo "Deleting old backup: $old_bkup"
+        rm -r "$old_bkup"
+        list_of_bkups="$(find "$bkup_dir" -name "*.$bkup_type" | sort)"
+    done
+    echo
 fi
 
 # Perform backup.
@@ -131,7 +130,7 @@ else
     rc=$?
 fi
 if [[ $rc -ne 0 ]]; then
-	cat "$bkup_dir/$bkup_date.$bkup_type/mongodump.log" >&2
+    cat "$bkup_dir/$bkup_date.$bkup_type/mongodump.log" >&2
 fi
 date -u +'finish: %F %T %Z'
 
@@ -178,7 +177,7 @@ if [[ -s "$log_err" || $post_backup_rc -gt 0 ]]; then
     exit 1
 else
     if [[ ! -z "$mail_on_success" ]]; then
-       	mail -s "Success - MongoDB Backup $HOSTNAME" "$mail_on_success" < "$log"
+        mail -s "Success - MongoDB Backup $HOSTNAME" "$mail_on_success" < "$log"
     fi
     exit 0
 fi

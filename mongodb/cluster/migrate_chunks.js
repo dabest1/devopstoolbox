bc9a9e8ce8900ab3d2cfeddda5280fdbd851beb4
Dimitriy Alekseyev <dimitriy.alekseyev@glu.com>
2016-07-15 16:31:12 -0700

    Added support for multiple collections to be processed.

--- mongodb/cluster/migrate_chunks.js
+++ mongodb/cluster/migrate_chunks.js
@@ -3,50 +3,54 @@
 // Original source:
 //     https://www.andrewzammit.com/blog/mongodb-migrate-and-merge-all-chunks-in-shard/
 
-var databaseName = 'database'; // your database name
-var collectionName = 'collection'; // your collection
-var adminUsername = ''; // user with clusterAdmin role
-var adminPassword = ''; // that user's password
-var sleepMsBetweenMoveChunk = 60000 // Sleep time in milliseconds
+var databaseName = 'database'; // Your database name.
+var collectionNames = ['collection1', 'collection2']; // Your collections.
+var adminUsername = ''; // User with clusterAdmin role.
+var adminPassword = ''; // That user's password.
+var sleepMsBetweenMoveChunk = 60000 // Sleep time in milliseconds.
 
-var version = '1.0.2'
+var version = '1.0.3'
 
-var namespace = databaseName+'.'+collectionName;
 var admindb = db.getSiblingDB('admin');
 var configdb = db.getSiblingDB('config');
 
 admindb.auth(adminUsername,adminPassword);
 
 if ( sh.getBalancerState() ) {
-	print('balancer is enabled, turn it off');
+	print('Balancer is enabled, turn it off.');
 	quit();
 }
 
 var foundDatabase = configdb.databases.findOne({_id:databaseName,partitioned:true});
 if ( !foundDatabase || !foundDatabase.primary ) {
-	print('no partitioned database found with name '+databaseName);
+	print('No partitioned database found with name '+databaseName);
 	quit();
 }
 
 var primaryShard = foundDatabase.primary;
-print('primary shard is '+primaryShard);
+print('Primary shard is '+primaryShard);
 
-var eligibleChunks = configdb.chunks.find({ns:namespace,shard:{$ne:primaryShard}});
-if ( eligibleChunks.count() === 0 ) {
-	print('no eligible chunks were found, either none exist or they are all already on the primary shard.');
-}
+collectionNames.forEach(function (collectionName, index, array) {
+	print('Collection: '+collectionName);
+	var namespace = databaseName+'.'+collectionName;
 
-var i = 0;
-eligibleChunks.forEach(function(chunk) {
-	i++;
-	print(i+' moving chunk id '+chunk._id.toString());
-	var result = admindb.runCommand({moveChunk:namespace,bounds:[chunk.min,chunk.max],to:primaryShard});
-	if ( !result || !result.ok ) {
-		print(i+' error moving chunk with id '+chunk._id.toString());
-		print(JSON.stringify(result));
-		quit();
+	var eligibleChunks = configdb.chunks.find({ns:namespace,shard:{$ne:primaryShard}});
+	if ( eligibleChunks.count() === 0 ) {
+		print('No eligible chunks were found, either none exist or they are all already on the primary shard.');
 	}
-	print(i+' moved chunk successfully');
-	print("Sleeping for "+sleepMsBetweenMoveChunk+" ms...")
-	sleep(sleepMsBetweenMoveChunk)
+
+	var i = 0;
+	eligibleChunks.forEach(function(chunk) {
+		i++;
+		print(i+' moving chunk id '+chunk._id.toString());
+		var result = admindb.runCommand({moveChunk:namespace,bounds:[chunk.min,chunk.max],to:primaryShard});
+		if ( !result || !result.ok ) {
+			print(i+' error moving chunk with id '+chunk._id.toString());
+			print(JSON.stringify(result));
+			quit();
+		}
+		print(i+' moved chunk successfully');
+		print("Sleeping for "+sleepMsBetweenMoveChunk+" ms...")
+		sleep(sleepMsBetweenMoveChunk)
+	});
 });

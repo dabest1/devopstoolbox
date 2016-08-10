4ce5c01521c9027740d2759543ee561cdf7ecf9b
dabest1 <dabest1@users.noreply.github.com>
2016-08-09 18:33:30 -0700

    Changed the order of output. Added description of where the output is comign from.

--- aws/aws_list_profiles.sh
+++ aws/aws_list_profiles.sh
@@ -4,10 +4,13 @@
 #     Lists all AWS profiles that are found in ~/.aws/config and 
 #     ~/.aws/credentials files.
 
-version=1.0.0
+version="1.0.1"
 
 echo "To switch AWS profile, run the following command followed by a profile name:"
 echo "export AWS_PROFILE="
 echo
-< ~/.aws/config grep '^\[.*\]$' | tr -d '[]' | sed 's/profile //'
+echo "~/.aws/credentials:"
 < ~/.aws/credentials grep '^\[.*\]$' | tr -d '[]'
+echo
+echo "~/.aws/config:"
+< ~/.aws/config grep '^\[.*\]$' | tr -d '[]' | sed 's/profile //'

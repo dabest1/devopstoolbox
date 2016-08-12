061bd5372f26e55d70ec1df91fd0fccf14647198
dabest1 <dabest1@users.noreply.github.com>
2016-08-12 12:56:46 -0700

    Added ability to format and mount a volume.

--- aws/ec2/ec2_create_volume.sh
+++ aws/ec2/ec2_create_volume.sh
@@ -2,10 +2,11 @@
 
 # Purpose:
 #     Create AWS volume and attach it.
+#     If mount point is provided, then format and mount the volume.
 # Usage:
-#     Run script with -h option to get usage.
+#     Run script with --help option to get usage.
 
-version="1.0.6"
+version="1.0.7"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -16,16 +17,19 @@ name="$1"
 volume_size="$2"
 volume_type="$3"
 device="$4"
+volume_mount="$5"
 profile="${AWS_PROFILE:-default}"
 
-if [[ -z $name || -z $volume_size || -z $volume_type || -z $device || $1 == "-h" ]]; then
+if [[ $1 == "--help" || -z $name || -z $volume_size || -z $volume_type || -z $device ]]; then
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name hostname volume_size_GiB volume_type device"
+    echo "    $script_name hostname volume_size_GiB volume_type device [volume_mount]"
     echo
     echo "Example:"
     echo "    $script_name my_host 1024 gp2 /dev/sdf"
+    echo "    or"
+    echo "    $script_name my_host 1024 gp2 /dev/sdf /data"
     exit 1
 fi
 
@@ -37,19 +41,20 @@ echo "hostname: $name" | tee -a $log
 echo "volume_size_GiB: $volume_size" | tee -a $log
 echo "volume_type: $volume_type" | tee -a $log
 echo "device: $device" | tee -a $log
+echo "volume_mount: $volume_mount" | tee -a $log
 echo | tee -a $log
 
 instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name,Values=$name" "Name=instance-state-name,Values=running" --query 'Reservations[].Instances[].[InstanceId]' --output text)
 rc=$?
 if [[ $rc != 0 ]]; then
-    echo "Error: Unable to query AWS."
+    echo "Error: Unable to query AWS." | tee -a $log
     exit 1
 fi
 
 result=$(aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id")
 rc=$?
 if [[ $rc != 0 ]]; then
-    echo "Error: Unable to query AWS."
+    echo "Error: Unable to query AWS." | tee -a $log
     exit 1
 fi
 availability_zone=$(echo "$result" | awk -F'"' '/"AvailabilityZone":/{print $4}')
@@ -58,7 +63,7 @@ echo "Create volume..." | tee -a $log
 result=$(aws --profile "$profile" ec2 create-volume --size "$volume_size" --availability-zone "$availability_zone" --volume-type "$volume_type")
 rc=$?
 if [[ $rc != 0 ]]; then
-    echo "Error: Unable to create volume."
+    echo "Error: Unable to create volume." | tee -a $log
     exit 1
 fi
 echo "$result" | tee -a $log
@@ -81,4 +86,28 @@ while [[ $attachment_state != "attached" ]]; do
     echo -n "."
     sleep 1
 done
+echo "Done." | tee -a $log
+
+if [[ -z $volume_mount ]]; then
+    exit 0
+fi
+
+echo | tee -a $log
+echo "Format new volume..." | tee -a $log
+ssh "$name" "sudo mkfs.ext4 $device" | tee -a $log
+rc=$?
+if [[ $rc -gt 0 ]]; then
+    echo "Error: Could not format volume." | tee -a $log
+    exit 1
+fi
+echo "Done." | tee -a $log
+echo | tee -a $log
+
+echo "Mount new volume..." | tee -a $log
+ssh "$name" "echo '$device $volume_mount ext4 defaults,auto,noatime,noexec 0 0' | sudo tee -a /etc/fstab > /dev/null; sudo mkdir $volume_mount; sudo mount -a" | tee -a $log
+rc=$?
+if [[ $rc -gt 0 ]]; then
+    echo "Error: Could not mount volume." | tee -a $log
+    exit 1
+fi
 echo "Done." | tee -a $log

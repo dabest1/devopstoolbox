ff123948f7819c99e30ca518355b5cba91e6c0a4
dabest1 <dabest1@users.noreply.github.com>
2016-09-09 17:39:34 -0700

    Changed log naming convention.

--- mongodb/cluster/manage_local_cluster/drop.sh
+++ mongodb/cluster/manage_local_cluster/drop.sh
@@ -3,8 +3,8 @@
 # Purpose:
 #     Stop MongoDB cluster and delete all database files and logs.
 
-version="1.0.1"
+version="1.0.2"
 
 ./stop.sh
 
-rm -rf a1 a2 a3 b1 b2 b3 c1 c2 c3 d1 d2 d3 cfg1 cfg2 cfg3 log.*
+rm -rf a1 a2 a3 b1 b2 b3 c1 c2 c3 d1 d2 d3 cfg1 cfg2 cfg3 *.log
--- mongodb/cluster/manage_local_cluster/start.sh
+++ mongodb/cluster/manage_local_cluster/start.sh
@@ -3,36 +3,36 @@
 # Purpose:
 #     Create and start MongoDB cluster.
 
-version="1.0.1"
+version="1.0.2"
 
 mkdir a1 a2 a3 b1 b2 b3 c1 c2 c3 d1 d2 d3 cfg1 cfg2 cfg3
 
 echo config servers
-mongod --configsvr --dbpath cfg1 --port 26050 --fork --logpath log.cfg1 --logappend
-mongod --configsvr --dbpath cfg2 --port 26051 --fork --logpath log.cfg2 --logappend
-mongod --configsvr --dbpath cfg3 --port 26052 --fork --logpath log.cfg3 --logappend
+mongod --configsvr --dbpath cfg1 --port 26050 --fork --logpath cfg1.log --logappend
+mongod --configsvr --dbpath cfg2 --port 26051 --fork --logpath cfg2.log --logappend
+mongod --configsvr --dbpath cfg3 --port 26052 --fork --logpath cfg3.log --logappend
 
 echo
 echo shard servers
-mongod --shardsvr --replSet a --dbpath a1 --logpath log.a1 --port 27000 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet a --dbpath a2 --logpath log.a2 --port 27001 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet a --dbpath a3 --logpath log.a3 --port 27002 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet b --dbpath b1 --logpath log.b1 --port 27100 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet b --dbpath b2 --logpath log.b2 --port 27101 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet b --dbpath b3 --logpath log.b3 --port 27102 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet c --dbpath c1 --logpath log.c1 --port 27200 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet c --dbpath c2 --logpath log.c2 --port 27201 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet c --dbpath c3 --logpath log.c3 --port 27202 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet d --dbpath d1 --logpath log.d1 --port 27300 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet d --dbpath d2 --logpath log.d2 --port 27301 --fork --logappend --smallfiles --oplogSize 50
-mongod --shardsvr --replSet d --dbpath d3 --logpath log.d3 --port 27302 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet a --dbpath a1 --logpath a1.log --port 27000 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet a --dbpath a2 --logpath a2.log --port 27001 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet a --dbpath a3 --logpath a3.log --port 27002 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet b --dbpath b1 --logpath b1.log --port 27100 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet b --dbpath b2 --logpath b2.log --port 27101 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet b --dbpath b3 --logpath b3.log --port 27102 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet c --dbpath c1 --logpath c1.log --port 27200 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet c --dbpath c2 --logpath c2.log --port 27201 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet c --dbpath c3 --logpath c3.log --port 27202 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet d --dbpath d1 --logpath d1.log --port 27300 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet d --dbpath d2 --logpath d2.log --port 27301 --fork --logappend --smallfiles --oplogSize 50
+mongod --shardsvr --replSet d --dbpath d3 --logpath d3.log --port 27302 --fork --logappend --smallfiles --oplogSize 50
 
 echo
 echo mongos processes
-mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath log.mongos1 --port 27017
-mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath log.mongos2 --port 26061
-mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath log.mongos3 --port 26062
-mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath log.mongos4 --port 26063
+mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath mongos1.log --port 27017
+mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath mongos2.log --port 26061
+mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath mongos3.log --port 26062
+mongos --configdb myhost:26050,myhost:26051,myhost:26052 --fork --logappend --logpath mongos4.log --port 26063
 
 echo
 echo process list

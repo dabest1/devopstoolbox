2ea3cf0696b8d85b8a870c2991b55b2e7b7b152c
dabest1 <dabest1@users.noreply.github.com>
2016-10-11 15:30:31 -0700

    Initial commit.

---
+++ aws/iam/iam_add_user_to_group.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Purpose:
+#     Add IAM user to group.
+# Usage:
+#     Run script with --help option to get usage.
+
+version="1.0.0"
+
+set -o pipefail
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_name="$(basename "$0")"
+
+username="$1"
+groupname="$2"
+profile="${AWS_PROFILE:-default}"
+
+function usage {
+    echo "Usage:"
+    echo "    export AWS_PROFILE=profile"
+    echo
+    echo "    $script_name username groupname"
+    echo
+    echo "Example:"
+    echo "    $script_name john.doe admins"
+    exit 1
+}
+
+if [[ $1 == "--help" || -z $1 || -z $2 ]]; then
+    usage
+fi
+
+aws --profile "$profile" iam create-user --user-name "$username" --group-name "$groupname"
---
+++ aws/iam/iam_create_access_key.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Purpose:
+#     Create access key for IAM user.
+# Usage:
+#     Run script with --help option to get usage.
+
+version="1.0.0"
+
+set -o pipefail
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+script_name="$(basename "$0")"
+
+username="$1"
+profile="${AWS_PROFILE:-default}"
+
+function usage {
+    echo "Usage:"
+    echo "    export AWS_PROFILE=profile"
+    echo
+    echo "    $script_name username"
+    echo
+    echo "Example:"
+    echo "    $script_name john.doe"
+    exit 1
+}
+
+if [[ $1 == "--help" || -z $1 ]]; then
+    usage
+fi
+
+aws --profile "$profile" iam create-access-key --user-name "$username"

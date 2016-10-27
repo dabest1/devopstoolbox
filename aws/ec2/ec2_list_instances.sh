de47b7ce17e9e23912325f971da6545f178494b2
dabest1 <dabest1@users.noreply.github.com>
2016-10-27 07:46:23 -0700

    Added ability to filter instances by tag value.

--- aws/ec2/ec2_list_instances.sh
+++ aws/ec2/ec2_list_instances.sh
@@ -7,47 +7,63 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.6"
+version="1.0.7"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 
-name="$1"
 profile="${AWS_PROFILE:-default}"
 
 function usage {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name [name]"
-    echo "    or"
-    echo "    $script_name 'partial_name*'"
+    echo "    $script_name {[name]|'partial_name*'|-v tag-value}"
+    echo
+    echo "Description:"
+    echo "    -v, --tag-value    List instances which have the provided tag value in any of the tag keys."
+    echo "    -h, --help         Display this help."
     exit 1
 }
 
-if [[ $1 == "--help" ]]; then
-    usage
-fi
+while test -n "$1"; do
+    case "$1" in
+    -h|--help)
+        usage
+        ;;
+    -v|--tag-value)
+        shift
+        tag_value="$1"
+        shift
+        ;;
+    *)
+        name="$1"
+        shift
+    esac
+done
 
 # Header row.
 header_row="account	name	instance_id	private_ip	region	type	state"
-
-# If name is not supplied, then we want all instances.
-if [[ -z $name ]]; then
-    name='*'
-fi
-
-if echo "$name" | grep -q 'i-'; then
-    instance_ids="$name"
-else
-    #echo "name: $name"
-    instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
-fi
-#echo "instance_ids:" $instance_ids
-if [[ -z $instance_ids ]]; then
-    exit 1
-fi
-
 echo "$header_row"
-aws --profile "$profile" ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, PrivateIpAddress, Placement.AvailabilityZone, InstanceType, State.Name]' --output text | sort | awk -v profile="$profile" '{print profile"\t"$0}'
+
+# If tag is not supplied, then search by name, else search by tag.
+if [[ -z $tag_value ]]; then
+    # If name is not supplied, then we want all instances.
+    if [[ -z $name ]]; then
+        name='*'
+    fi
+
+    if echo "$name" | grep -q 'i-'; then
+        instance_ids="$name"
+    else
+        instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+    fi
+    if [[ -z $instance_ids ]]; then
+        exit 1
+    fi
+
+    aws --profile "$profile" ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, PrivateIpAddress, Placement.AvailabilityZone, InstanceType, State.Name]' --output text | sort | awk -v profile="$profile" '{print profile"\t"$0}'
+else
+    aws --profile "$profile" ec2 describe-instances --filter "Name=tag-value,Values=$tag_value" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, PrivateIpAddress, Placement.AvailabilityZone, InstanceType, State.Name]' --output text | sort | awk -v profile="$profile" '{print profile"\t"$0}'
+fi

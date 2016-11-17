26da85478bf874119e674d3f6780e951f83d4994
dabest1 <dabest1@users.noreply.github.com>
2016-11-16 16:15:23 -0800

    Changed ssh command to use current user or the one configured in SSH config file. Added comments.

--- ssh/add_public_key.sh
+++ ssh/add_public_key.sh
@@ -7,9 +7,11 @@
 #     add_public_key.sh host1 [host2 ...]
 ################################################################################
 
-version="1.0.0"
+version="1.0.1"
 
+# Hosts on which to add public key.
 hosts=$@
+# User to which to add public key. Current user or SSH config setting will control which user is used for SSH connectivity.
 user="your_username"
 public_key_file=~/.ssh/your_public_key.pub
 
@@ -17,9 +19,9 @@ public_key="$(cat "$public_key_file")"
 
 for host in $hosts; do
     echo "host: $host"
-    cat <<HERE_DOCUMENT | ssh "$user"@"$host"
-if ! grep -q "$public_key" /home/"$user"/.ssh/authorized_keys; then
-    echo "$public_key" >> /home/"$user"/.ssh/authorized_keys
+    cat <<HERE_DOCUMENT | ssh "$host"
+if ! sudo grep -q "$public_key" /home/"$user"/.ssh/authorized_keys; then
+    echo "$public_key" | sudo tee -a /home/"$user"/.ssh/authorized_keys > /dev/null
 fi
 HERE_DOCUMENT
 done

f03e1f9c62f5487bb9d14b858bc24e51652c7da9
dabest1 <dabest1@users.noreply.github.com>
2016-12-21 17:54:21 -0800

    Fixes based on lint suggestions.

--- aws/ec2/ec2_stop_instance.sh
+++ aws/ec2/ec2_stop_instance.sh
@@ -5,7 +5,7 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.6"
+version="1.0.7"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,7 +21,7 @@ function usage {
     echo "Description:"
     echo "    -f, --force   Do not prompt for confirmation."
     echo "    -w, --wait    Wait for 'stopped' state before finishing."
-    echo "    -h, --help    Show this help."
+    echo "    -h, --help    Display this help."
     exit 1
 }
 
@@ -49,51 +49,49 @@ if [[ -z $name ]]; then
     usage
 fi
 
-echo >> $log
-echo >> $log
-date +'%F %T %z' >> $log
-echo "profile: $profile" | tee -a $log
+echo >> "$log"
+echo >> "$log"
+date +'%F %T %z' >> "$log"
+echo "profile: $profile" | tee -a "$log"
 if echo "$name" | grep -q '^i-'; then
     instance_id="$name"
 else
-    echo "name: $name" | tee -a $log
+    echo "name: $name" | tee -a "$log"
     instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
     rc=$?
     if [[ $rc -gt 0 ]]; then
-        echo "Error: Failed to query AWS." | tee -a $log
+        echo "Error: Failed to query AWS." | tee -a "$log"
         exit 1
     fi
 fi
-echo "instance_id: $instance_id" | tee -a $log
+echo "instance_id: $instance_id" | tee -a "$log"
 
 aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
 rc=$?
 if [[ $rc -gt 0 ]]; then
-    echo "Error: Failed to query AWS." | tee -a $log
+    echo "Error: Failed to query AWS." | tee -a "$log"
     exit 1
 fi
-aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> $log
+aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
 
-aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a $log
-
-volume_ids=$(aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" "Name=attachment.delete-on-termination, Values=false" --query 'Volumes[*].[VolumeId]' --output text)
+aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
 
 if [[ $do_not_prompt != "yes" ]]; then
     echo -n 'Are you sure that you want this instance stopped? y/n: '
-    read yn
+    read -r yn
     if [[ $yn != y ]]; then
-        echo 'Aborted!' | tee -a $log
+        echo 'Aborted!' | tee -a "$log"
         exit 1
     fi
     echo
 fi
 
-echo "Stop instance..." | tee -a $log
-aws --profile "$profile" ec2 stop-instances --instance-ids "$instance_id" --output table | tee -a $log
+echo "Stop instance..." | tee -a "$log"
+aws --profile "$profile" ec2 stop-instances --instance-ids "$instance_id" --output table | tee -a "$log"
 state=""
 while [[ $state != "stopped" ]] && [[ $wait_for_stopped == yes ]]; do
     state=$(aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[State.Name]' --output text)
     echo -n "."
     sleep 1
 done
-echo 'Done.' | tee -a $log
+echo 'Done.' | tee -a "$log"
--- aws/ec2/ec2_terminate_instance.sh
+++ aws/ec2/ec2_terminate_instance.sh
@@ -5,7 +5,7 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.5"
+version="1.0.6"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -21,7 +21,7 @@ function usage {
     echo
     echo "Description:"
     echo "    -f, --force   Do not prompt for confirmation."
-    echo "    -h, --help    Show this help."
+    echo "    -h, --help    Display this help."
     exit 1
 }
 
@@ -45,57 +45,57 @@ if [[ -z $name ]]; then
     usage
 fi
 
-echo >> $log
-echo >> $log
-date +'%F %T %z' >> $log
-echo "profile: $profile" | tee -a $log
+echo >> "$log"
+echo >> "$log"
+date +'%F %T %z' >> "$log"
+echo "profile: $profile" | tee -a "$log"
 if echo "$name" | grep -q '^i-'; then
     instance_id="$name"
 else
-    echo "name: $name" | tee -a $log
+    echo "name: $name" | tee -a "$log"
     instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
 fi
-echo "instance_id: $instance_id" | tee -a $log
+echo "instance_id: $instance_id" | tee -a "$log"
 if [[ -z $instance_id ]]; then
     exit 1
 fi
 
 aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
-aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> $log
+aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
 
-aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a $log
+aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
 
 volume_ids=$(aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" "Name=attachment.delete-on-termination, Values=false" --query 'Volumes[*].[VolumeId]' --output text)
 
 if [[ $do_not_prompt != "yes" ]]; then
     echo -n "Are you sure that you want this instance terminated? y/n: "
-    read yn
+    read -r yn
     if [[ $yn != y ]]; then
-        echo "Aborted!" | tee -a $log
+        echo "Aborted!" | tee -a "$log"
         exit 1
     fi
     echo
 fi
 
-aws --profile "$profile" ec2 terminate-instances --instance-ids "$instance_id" --output table | tee -a $log
+aws --profile "$profile" ec2 terminate-instances --instance-ids "$instance_id" --output table | tee -a "$log"
 
 if [[ -z $volume_ids ]]; then
     exit 0
 fi
-echo "volume_ids:" $volume_ids | tee -a $log
+echo "volume_ids:" $volume_ids | tee -a "$log"
 
 if [[ $do_not_prompt != "yes" ]]; then
     echo -n "Are you sure that you want these volumes deleted? y/n: "
-    read yn
+    read -r yn
     if [[ $yn != y ]]; then
-        echo "Aborted!" | tee -a $log
+        echo "Aborted!" | tee -a "$log"
         exit 1
     fi
     echo
 fi
 
 for volume_id in $volume_ids; do
-    echo "Deleting volume_id: $volume_id" | tee -a $log
+    echo "Deleting volume_id: $volume_id" | tee -a "$log"
     aws --profile "$profile" ec2 delete-volume --volume-id "$volume_id" &> /dev/null
     return_code=$?
     while [[ $return_code -eq 255 ]]; do # Wait until the instance is terminated to delete the volume.
@@ -105,11 +105,11 @@ for volume_id in $volume_ids; do
         return_code=$?
     done
     if [[ $return_code -ne 0 ]]; then
-        echo "Error: Could not delete volume." | tee -a $log
-        echo "return_code: $return_code" | tee -a $log
+        echo "Error: Could not delete volume." | tee -a "$log"
+        echo "return_code: $return_code" | tee -a "$log"
         failures+=1
     else
-        echo "done" | tee -a $log
+        echo "done" | tee -a "$log"
     fi
 done
 

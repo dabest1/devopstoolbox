fa401c5b6bce445836aa7c70a6dc36456a4a36c7
dabest1 <dabest1@users.noreply.github.com>
2017-02-23 02:14:24 -0800

    Added status command. Moved start command into a function.

--- mongodb/backup/mongodb_restore.sh
+++ mongodb/backup/mongodb_restore.sh
@@ -4,7 +4,7 @@
 #     Restore MongoDB database.
 ################################################################################
 
-version="1.0.5"
+version="1.0.6"
 
 start_time="$(date -u +'%FT%TZ')"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -13,7 +13,8 @@ script_name="$(basename "$0")"
 # Variables.
 s3_download_script="$script_dir/s3_download.sh"
 restore_dir="/backups/restore"
-mongorestore="/usr/bin/mongorestore"
+mongorestore="$(which mongorestore)"
+mongod="$(which mongod)"
 
 # Process options.
 while [[ -n $1 ]]; do
@@ -34,6 +35,14 @@ while [[ -n $1 ]]; do
         s3_profile="$1"
         shift
         ;;
+    start)
+        command="start"
+        shift
+        ;;
+    status)
+        command="status"
+        shift
+        ;;
     *|--help)
         usage
     esac
@@ -44,12 +53,16 @@ done
 # Usage.
 usage() {
     echo "Usage:"
-    echo "    $script_name -b backup_to_restore -s s3_bucket_path -p s3_profile"
+    echo "    $script_name start -b backup_to_restore -s s3_bucket_path -p s3_profile"
+    echo "    or"
+    echo "    $script_name status -b backup_to_restore -s s3_bucket_path -p s3_profile"
     echo
     echo "Example:"
-    echo "    $script_name -b 20170101T010101Z.daily -s s3://dba-backup/mongodb/myhost -p dba-backup"
+    echo "    $script_name start -b 20170101T010101Z.daily -s s3://dba-backup/mongodb/myhost -p dba-backup"
     echo
     echo "Description:"
+    echo "    start                      Start a restore job."
+    echo "    status                     Return status of a restore job."
     echo "    -b, --backup_to_restore    Dated subdirecotry of a backup to restore."
     echo "    -s, --s3_bucket_path       AWS S3 bucket path of where the backup is located."
     echo "    -p, --s3_profile           AWS S3 profile to use."
@@ -122,20 +135,73 @@ verify_uuid() {
     echo "Done."
 }
 
-echo "**************************************************"
-echo "* Restore MongoDB"
-echo "* Time started: $start_time"
-echo "**************************************************"
+# Start restore job.
+start() {
+    echo "**************************************************"
+    echo "* Restore MongoDB"
+    echo "* Time started: $start_time"
+    echo "**************************************************"
+    echo
+    echo "Hostname: $HOSTNAME"
+    echo "MongoDB version: $("$mongod" --version | head -1)"
+    echo
+
+    mkdir "$restore_path" || exit 1
+
+    # Create restore status and pid file.
+    echo "$BASHPID" > "$restore_pid_file"
+    echo "{\"start_time\":\"$start_time\",\"backup_to_restore\":\"$backup_to_restore\",\"status\":\"running\"}" > "$restore_status_file"
+
+    get_backup
+    verify_md5
+    uncompress
+    restore
+    verify_uuid
+
+    end_time="$(date -u +'%FT%TZ')"
+    echo "**************************************************"
+    echo "* Time finished: $end_time"
+    echo "**************************************************"
+}
+
+# Get restore job status.
+status() {
+    local pid
+    local rc
+    local status
+
+    if [[ -f $restore_pid_file ]] && [[ -f $restore_status_file ]]; then
+        pid="$(cat "$restore_pid_file")"
+        kill -0 "$pid" 2> /dev/null
+        rc=$?
+        if [[ $rc -eq 0 ]]; then
+            # Show status.
+            cat "$restore_status_file"
+            exit 0
+        else
+            status="$(jq '.status' < "$restore_status_file" | tr -d '"')"
+            if [[ $status = "completed" ]]; then
+                # Show status.
+                cat "$restore_status_file"
+                exit 0
+            else
+                # Show status.
+                echo "{\"backup_to_restore\":\"$backup_to_restore\",\"status\":\"failed\"}"
+                exit 0
+            fi
+        fi
+    else
+        # Show status.
+        echo "{\"backup_to_restore\":\"$backup_to_restore\",\"status\":\"unknown\"}"
+    fi
+}
 
 restore_path="$restore_dir/$backup_to_restore"
+restore_pid_file="$restore_path/restore.pid"
+restore_status_file="$restore_path/restore.status.json"
 
-get_backup
-verify_md5
-uncompress
-restore
-verify_uuid
-
-end_time="$(date -u +'%FT%TZ')"
-echo "**************************************************"
-echo "* Time finished: $end_time"
-echo "**************************************************"
+if [[ $command = start ]]; then
+    start
+elif [[ $command = status ]]; then
+    status
+fi

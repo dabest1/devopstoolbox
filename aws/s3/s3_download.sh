d31a7e0d87ad6e152c7d5c2ca772196d16589870
dabest1 <dabest1@users.noreply.github.com>
2017-03-02 13:55:19 -0800

    Add error handling.

--- aws/s3/s3_download.sh
+++ aws/s3/s3_download.sh
@@ -4,23 +4,19 @@
 #     Download data from AWS S3.
 ################################################################################
 
-version="1.0.0"
+version="1.0.1"
 
 start_time="$(date -u +'%FT%TZ')"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 config_path="$script_dir/${script_name/.sh/.cfg}"
 
-aws_profile="$1"
-s3_bucket_path="$2"
-dir_target="$3"
+# Functions.
 
-if ! echo "$s3_bucket_path" | grep -q '/$'; then
-    s3_bucket_path="$s3_bucket_path/"
-fi
+shopt -s expand_aliases
+alias die='error_exit "ERROR in $0: line $LINENO:"'
 
-s3cmd="aws --profile $aws_profile s3"
-
+# Usage.
 usage() {
     echo "Usage:"
     echo "    $script_name aws_profile s3_bucket_path dir_target"
@@ -30,12 +26,29 @@ usage() {
     exit 1
 }
 
+error_exit() {
+    echo "$@" >&2
+    exit 78
+}
+
+set -o pipefail
+set -o errtrace
+trap 'rc=$? && [[ $rc -ne 78 ]] && error_exit "ERROR in $0: line $LINENO: exit code $rc." || exit 78' ERR
+trap 'error_exit "ERROR in $0: Received signal SIGHUP."' SIGHUP
+trap 'error_exit "ERROR in $0: Received signal SIGINT."' SIGINT
+trap 'error_exit "ERROR in $0: Received signal SIGTERM."' SIGTERM
+
+aws_profile="$1"
+s3_bucket_path="$2"
+dir_target="$3"
+
 if [[ $# -ne 3 ]]; then
     usage
 fi
 
-set -E
-set -o pipefail
+if ! echo "$s3_bucket_path" | grep -q '/$'; then
+    s3_bucket_path="$s3_bucket_path/"
+fi
 
 echo "**************************************************"
 echo "* Download from S3"
@@ -50,12 +63,9 @@ if [[ ! -d $dir_target ]]; then
     mkdir "$dir_target"
 fi
 echo 'Download from S3:'
-$s3cmd sync "$s3_bucket_path" "$dir_target"
-
-#echo
-#echo 'Create md5 check sums.'
-#cd "$dir_to_upload" || exit 1
-#find . -type f | grep -v '[.]log$' | grep -v '[.]err$' | grep -v 'md5sum.txt' | sort | xargs md5sum > md5sum.txt
+aws --profile "$aws_profile" s3 sync "$s3_bucket_path" "$dir_target"
+rc=$?
+if [[ $rc -ne 0 ]]; then die "Download from S3 failed."; fi
 
 echo "**************************************************"
 echo "* Time finished: $(date -u +'%FT%TZ')"

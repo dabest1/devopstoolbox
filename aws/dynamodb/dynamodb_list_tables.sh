44cf7267e8455c8abd23f166c9c9c32f49095f47
dabest1 <dabest1@users.noreply.github.com>
2017-03-02 18:18:24 -0800

    Improved usage information. Added version option.

--- aws/dynamodb/dynamodb_list_tables.sh
+++ aws/dynamodb/dynamodb_list_tables.sh
@@ -1,24 +1,52 @@
 #!/bin/bash
-
+################################################################################
 # Purpose:
-#     Provides a list of DynamoDB tables. AWS paginates the output at 100 items per page.
+#     Provides a list of DynamoDB tables. AWS paginates the output at 100 items 
+#     per page.
 # Usage:
-#     Run script with -h option to get usage.
+#     Run script with --help option to get usage.
+################################################################################
 
-version=1.0.0
+version="1.1.0"
 
-set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 
+# Functions.
+
+# Usage.
+usage() {
+    echo "Usage:"
+    echo "    export AWS_PROFILE=profile"
+    echo
+    echo "    $script_name"
+    echo
+    echo "Description:"
+    echo "    Running the script with no options will display a list of DynamoDB tables."
+    echo "    Output is paginated at 100 items per page."
+    echo "    If AWS_PROFILE is not exported, then 'default' profile will be used."
+    echo "    "
+    echo "    Options:"
+    echo "    --version                  Display script version."
+    echo "    --help                     Display this help."
+    exit 1
+}
+
+set -o pipefail
+
 profile="${AWS_PROFILE:-default}"
 
-if [[ $1 == '-h' ]]; then
-    echo 'Usage:'
-    echo '    export AWS_PROFILE=profile'
-    echo "    $script_name"
-    exit 1
-fi
+# Process options.
+while [[ -n $1 ]]; do
+    case "$1" in
+    --version)
+        echo "version: $version"
+        exit
+        ;;
+    *|--help)
+        usage
+    esac
+done
 
 echo "profile: $profile"
 echo

92743dd97138b34d71801c9d87a776a495fb0160
dabest1 <dabest1@users.noreply.github.com>
2017-10-18 20:14:21 -0700

    Added column formatting.

--- aws/ec2/ec2_list_volumes.sh
+++ aws/ec2/ec2_list_volumes.sh
@@ -6,7 +6,7 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.3"
+version="1.1.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -34,19 +34,22 @@ fi
 
 echo "profile: $profile"
 echo
-echo "DeleteOnTermination Device InstanceId Size State VolumeId"
 
-if echo "$name" | grep -q '^i-'; then
-    instance_id="$name"
-    aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output text
-elif [[ $name == '*' ]]; then
-    aws --profile "$profile" ec2 describe-volumes --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output text
-else
-    instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
-    if [[ -z $instance_ids ]]; then
-        exit 1
-    fi
-    for instance_id in $instance_ids; do
+{
+    echo "DeleteOnTermination Device InstanceId Size State VolumeId"
+
+    if echo "$name" | grep -q '^i-'; then
+        instance_id="$name"
         aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output text
-    done
-fi
+    elif [[ $name == '*' ]]; then
+        aws --profile "$profile" ec2 describe-volumes --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output text
+    else
+        instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+        if [[ -z $instance_ids ]]; then
+            exit 1
+        fi
+        for instance_id in $instance_ids; do
+            aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output text
+        done
+    fi
+} | column -t

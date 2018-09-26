eada9216062d331257dafe4e41340adcf941fce1
dabest1 <dabest1@users.noreply.github.com>
2018-09-26 13:52:08 -0700

    Do not exit if account and region have 0 EC2 instances.

--- aws/inventory/ec2_inventory.sh
+++ aws/inventory/ec2_inventory.sh
@@ -6,7 +6,7 @@
 #     Run script with --help option to get usage.
 ################################################################################
 
-version="1.5.0"
+version="1.5.1"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -56,8 +56,9 @@ refresh_subtask() {
         echo "profile: $profile"
         name='*'
         instance_ids=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
-        if [[ -z $instance_ids ]]; then
-            exit 1
+        rc="$?"
+        if [[ $rc -gt 0 ]]; then
+            failures=$((failures + 1))
         fi
 
         aws --profile "$profile" ec2 describe-instances --instance-ids $instance_ids --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, PrivateIpAddress, PublicIpAddress, KeyName, Placement.AvailabilityZone, InstanceType, State.Name]' --output text | sort | awk -v profile="$profile" '{print profile"\t"$0}' >> "$data_tmp_path"

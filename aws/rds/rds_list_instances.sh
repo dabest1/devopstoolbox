a885c77fa3b0d372de0c2a0a7039b2ecb08ad01f
dabest1 <dabest1@users.noreply.github.com>
2018-09-27 01:03:32 -0700

    Accept profile and region options. Display output in table format. Modify column header.

--- aws/rds/rds_list_instances.sh
+++ aws/rds/rds_list_instances.sh
@@ -5,29 +5,48 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.1"
+version="2.0.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 
-name="$1"
 profile="${AWS_PROFILE:-default}"
 
 function usage {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name [name]"
+    echo "    $script_name [--profile profile] [--region region] [name]"
     exit 1
 }
 
-if [[ $1 == "--help" ]]; then
-    usage
-fi
+while test -n "$1"; do
+    case "$1" in
+    -h|--help)
+        usage
+        ;;
+    --profile)
+        shift
+        profile="$1"
+        shift
+        ;;
+    --region)
+        shift
+        region="$1"
+        region_opt="--region=$region"
+        shift
+        ;;
+    *)
+        name="$1"
+        shift
+    esac
+done
 
 # Header row.
-header_row="account	name	engine	region	class	status	address	port"
+header_row="account name engine region class status endpoint port"
 
-echo "$header_row"
-aws --profile "$profile" rds describe-db-instances --db-instance-identifier "$name" | jq '.DBInstances[] | [.DBInstanceIdentifier, .Engine, .AvailabilityZone, .DBInstanceClass, .DBInstanceStatus, .Endpoint.Address, .Endpoint.Port] | @tsv' | tr -d '"' | sed 's/\\t/	/g' | awk -v profile="$profile" '{print profile"\t"$0}'
+{
+    echo "$header_row"
+    aws --profile "$profile" rds describe-db-instances --db-instance-identifier "$name" | jq '.DBInstances[] | [.DBInstanceIdentifier, .Engine, .AvailabilityZone, .DBInstanceClass, .DBInstanceStatus, .Endpoint.Address, .Endpoint.Port] | @tsv' | tr -d '"' | sed 's/\\t/ /g' | awk -v profile="$profile" '{print profile" "$0}'
+} | column -t

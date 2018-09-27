73085fa627194ff9faa9afffc6cf7e842a2b8d54
dabest1 <dabest1@users.noreply.github.com>
2018-09-27 16:11:45 -0700

    Added ability to delete by snapshot ID.

--- aws/ec2/ec2_delete_snapshot.sh
+++ aws/ec2/ec2_delete_snapshot.sh
@@ -5,7 +5,7 @@
 # Usage:
 #     Run script with -h option to get usage.
 
-version="1.0.0"
+version="1.1.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -16,7 +16,7 @@ function usage {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name snapshot_descr"
+    echo "    $script_name {snapshot_id | snapshot_description}"
     echo
     echo "Description:"
     echo "    -h, --help      Show this help."
@@ -29,13 +29,13 @@ while test -n "$1"; do
         usage
         ;;
     *)
-        snapshot_descr="$1"
+        snapshot="$1"
         shift
     esac
 done
 profile="${AWS_PROFILE:-default}"
 
-if [[ -z $snapshot_descr ]]; then
+if [[ -z $snapshot ]]; then
     usage
 fi
 
@@ -43,30 +43,49 @@ echo >> $log
 echo >> $log
 date +'%F %T %z' >> $log
 echo "profile: $profile" | tee -a $log
-echo "snapshot_descr: $snapshot_descr" | tee -a $log
-echo | tee -a $log
 
-result="$(aws --profile "$profile" ec2 describe-snapshots --filters "Name=description,Values=$snapshot_descr" --output json)"
-rc=$?
-if [[ $rc -ne 0 ]]; then
-    echo 'Error getting snapshot information.' | tee -a $log
-    exit 1
-fi
-echo $result | tee -a $log
+if echo "$snapshot" | grep -q '^snap-'; then
+    snapshot_id="$snapshot"
+    echo "snapshot_id: $snapshot_id" | tee -a $log
+    echo | tee -a $log
 
-snapshot_ids="$(echo $result | jq -r '.Snapshots[].SnapshotId')"
-
-echo
-echo "Delete snapshots..." | tee -a $log
-echo "snapshot_ids: 
-$snapshot_ids" | tee -a $log
-for snapshot_id in $snapshot_ids; do
+    echo
+    echo "Delete snapshots..." | tee -a $log
+    echo "snapshot_ids:
+$snapshot_id" | tee -a $log
     aws --profile "$profile" ec2 delete-snapshot --snapshot-id "$snapshot_id"
     rc=$?
     if [[ $rc -ne 0 ]]; then
         echo 'Error deleting snapshot.' | tee -a $log
         exit 1
     fi
-done
+else
+    snapshot_descr="$snapshot"
+    echo "snapshot_descr: $snapshot_descr" | tee -a $log
+    echo | tee -a $log
+
+    result="$(aws --profile "$profile" ec2 describe-snapshots --filters "Name=description,Values=$snapshot_descr" --output json)"
+    rc=$?
+    if [[ $rc -ne 0 ]]; then
+        echo 'Error getting snapshot information.' | tee -a $log
+        exit 1
+    fi
+    echo $result | tee -a $log
+
+    snapshot_ids="$(echo $result | jq -r '.Snapshots[].SnapshotId')"
+
+    echo
+    echo "Delete snapshots..." | tee -a $log
+    echo "snapshot_ids:
+$snapshot_ids" | tee -a $log
+    for snapshot_id in $snapshot_ids; do
+        aws --profile "$profile" ec2 delete-snapshot --snapshot-id "$snapshot_id"
+        rc=$?
+        if [[ $rc -ne 0 ]]; then
+            echo 'Error deleting snapshot.' | tee -a $log
+            exit 1
+        fi
+    done
+fi
 
 echo 'Done.' | tee -a $log

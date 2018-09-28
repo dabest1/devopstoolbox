41862a4ed3db45b95138b556128f6c3830e78281
dabest1 <dabest1@users.noreply.github.com>
2018-09-28 05:28:42 -0700

    Added deletion from source bucket, so that same data is not stored in both - source and destination buckets.

--- aws/s3/s3_transfer_backups.sh
+++ aws/s3/s3_transfer_backups.sh
@@ -6,7 +6,7 @@
 #         s3://bucket/database_type/hostname/timestamp.backup_type/*
 ################################################################################
 
-version="1.0.0"
+version="1.1.0"
 
 start_time="$(date -u +'%F %T %Z')"
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,7 +17,9 @@ config_path="$script_dir/${script_name/.sh/.cfg}"
 source "$config_path"
 
 s3cmd="aws --profile $aws_profile s3"
-backup_types="daily weekly monthly yearly"
+
+shopt -s expand_aliases
+alias die='error_exit "ERROR: ${0}(@$LINENO):"'
 
 function usage {
     echo "Usage:"
@@ -27,6 +29,15 @@ function usage {
     echo "    $script_name"
     exit 1
 }
+
+error_exit() {
+    echo
+    echo "$@" >&2
+    exit 1
+}
+trap "error_exit 'Received signal SIGHUP'" SIGHUP
+trap "error_exit 'Received signal SIGINT'" SIGINT
+trap "error_exit 'Received signal SIGTERM'" SIGTERM
 
 if [[ $# -ne 0 ]]; then
     usage
@@ -50,60 +61,47 @@ echo "S3 target: $s3uri_target"
 echo
 echo "Hosts:"
 s3_host_list="$($s3cmd ls "$s3uri_source" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort)"
+if [[ $? -ne 0 ]]; then
+    die "Listing in S3 source bucket has failed."
+fi
+s3_host_list="$($s3cmd ls "$s3uri_source" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort | head -1)" # DEBUG
 echo "$s3_host_list"
 
 echo
 for host in $s3_host_list; do
     echo "$host source:"
-    s3_backup_list_source="$($s3cmd ls "$s3uri_source$host/" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort)"
+    s3_backup_list_source="$($s3cmd ls "${s3uri_source}${host}/" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort)"
     echo "$s3_backup_list_source"
     echo
 
     echo "$host target:"
-    s3_backup_list_target="$($s3cmd ls "$s3uri_target$host/" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort)"
+    s3_backup_list_target="$($s3cmd ls "${s3uri_target}${host}/" | awk -F' |/' '/ PRE / {print $(NF-1)}' | sort)"
     echo "$s3_backup_list_target"
-    echo
 
     # Skip the most recent backup, as the upload to S3 source location may not have completed.
     s3_backup_list_source="$(echo "$s3_backup_list_source" | sort -r | sed '1d' | sort)"
 
     for s3_backup in $s3_backup_list_source; do
-        echo "Transfer: $s3_backup"
+        echo
         if ! echo "$s3_backup_list_target" | grep -q "$s3_backup"; then
-            $s3cmd cp --recursive "$s3uri_source$host/$s3_backup" "$s3uri_target$host/$s3_backup"
-            rc=$?
-            echo
-            if [[ $rc -ne 0 ]]; then
-                echo "Error: S3 copy failed!"
-                exit 1
+            echo "Transfer: ${s3uri_source}${host}/$s3_backup"
+            $s3cmd cp --recursive --quiet "${s3uri_source}${host}/$s3_backup" "${s3uri_target}${host}/$s3_backup"
+            if [[ $? -ne 0 ]]; then
+                die "S3 copy failed."
             fi
 
-            # TODO: Delete in source bucket after copying to target bucket.
+            # Delete from source bucket after copying to target bucket.
+            echo "Delete: ${s3uri_source}${host}/$s3_backup"
+            $s3cmd rm --recursive --quiet "${s3uri_source}${host}/$s3_backup"
+            if [[ $? -ne 0 ]]; then
+                die "Deleting from S3 source bucket has failed."
+            fi
         else
-            echo "Skipped transfer as this timestamped directory is in both buckets."
-            echo
+            # Skip transfer if the timestamped directory is in both buckets.
+            echo "Skip transfer: ${s3uri_source}${host}/$s3_backup"
         fi
     done
-
-    # for backup_type in $backup_types; do
-    #     backup_list="$(echo "$s3_backup_list" | grep "[.]$backup_type" | sort -r)"
-    #     if [[ $backup_type == 'daily' ]]; then
-    #         num_bkups=$num_daily_bkups
-    #     elif [[ $backup_type == 'weekly' ]]; then
-    #         num_bkups=$num_weekly_bkups
-    #     elif [[ $backup_type == 'monthly' ]]; then
-    #         num_bkups=$num_monthly_bkups
-    #     elif [[ $backup_type == 'yearly' ]]; then
-    #         num_bkups=$num_yearly_bkups
-    #     fi
-    #     num_bkups=$((num_bkups + 1))
-    #     #backup_delete="$(echo "$backup_list" | sed -n "${num_bkups},100p")"
-    #     #for backup in $backup_delete; do
-    #         #echo "delete backup: $backup"
-    #         #$s3cmd rm --recursive --quiet "$s3_bucket_path$host/$backup"
-    #     #done
-    # done
-    # echo
+    echo
 done
 
 echo "**************************************************"

7314ba57f82d6bcff7ad4e63f0249801569df360
dabest1 <dabest1@users.noreply.github.com>
2018-12-03 12:12:21 -0800

    Added ability to pass instance name. It can now accept multiple instances at once.

--- aws/autoscaling/autoscaling_mark_instance_unhealthy.sh
+++ aws/autoscaling/autoscaling_mark_instance_unhealthy.sh
@@ -5,28 +5,64 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.0.0"
+version="1.1.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 
-instance_id="$1"
 profile="${AWS_PROFILE:-default}"
 
 function usage {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name instance_id"
+    echo "    $script_name {name | instance_id} ..."
     echo
     echo "Example:"
     echo "    $script_name i-abcd1234"
+    echo
+    echo "Description:"
+    echo "    -h, --help         Display this help."
     exit 1
 }
 
-if [[ $1 == "--help" || -z $1 ]]; then
+while test -n "$1"; do
+    case "$1" in
+    -h|--help)
+        usage
+        ;;
+    *)
+        if [[ -z $names ]]; then
+            names="$1"
+        else
+            names="$names $1"
+        fi
+        shift
+    esac
+done
+
+if [[ -z $names ]]; then
     usage
 fi
 
-aws --profile "$profile" autoscaling set-instance-health --instance-id "$instance_id" --health-status "Unhealthy"
+for name in $names; do
+    if echo "$name" | grep -q '^i-'; then
+        instance_id="$name"
+    else
+        echo "name: $name"
+        instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+        if [[ $? -ne 0 ]]; then
+            echo "Error: Failed to query AWS."
+            exit 1
+        fi
+    fi
+    echo "instance_id: $instance_id"
+
+    aws --profile "$profile" autoscaling set-instance-health --instance-id "$instance_id" --health-status "Unhealthy"
+    if [[ $? -ne 0 ]]; then
+        echo "Error: Failed to set health status."
+        exit 1
+    fi
+    echo
+done

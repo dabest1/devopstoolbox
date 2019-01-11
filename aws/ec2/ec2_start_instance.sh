3bad06a7df320d7fda1a93a3846db1b0cc8a0717
dabest1 <dabest1@users.noreply.github.com>
2019-01-11 11:47:28 -0800

    Added profile and region options.

--- aws/ec2/ec2_start_instance.sh
+++ aws/ec2/ec2_start_instance.sh
@@ -5,20 +5,24 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.1.0"
+version="1.2.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 log="$script_dir/${script_name/.sh/.log}"
 
+profile="${AWS_PROFILE:-default}"
+
 usage() {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name [-n] [-w] {name | instance_id}..."
+    echo "    $script_name [--profile profile] [--region region] [-n] [-w] {name | instance_id}..."
     echo
     echo "Description:"
+    echo "    --profile          Use a specified profile from your AWS credential file, otherwise get it from AWS_PROFILE variable."
+    echo "    --region           Use a specified region instead of region from configuration or environment setting."
     echo "    -n, --no-prompt    No confirmation prompt."
     echo "    -w, --wait         Wait for 'running' state before finishing."
     echo "    -h, --help         Display this help."
@@ -27,6 +31,17 @@ usage() {
 
 while test -n "$1"; do
     case "$1" in
+    --profile)
+        shift
+        profile="$1"
+        shift
+        ;;
+    --region)
+        shift
+        region="$1"
+        region_opt="--region=$region"
+        shift
+        ;;
     -h|--help)
         usage
         ;;
@@ -47,7 +62,6 @@ while test -n "$1"; do
         shift
     esac
 done
-profile="${AWS_PROFILE:-default}"
 
 if [[ -z $names ]]; then
     usage
@@ -64,7 +78,7 @@ for name in $names; do
         instance_id="$name"
     else
         echo "name: $name" | tee -a "$log"
-        instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+        instance_id=$(aws --profile "$profile" $region_opt ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
         rc=$?
         if [[ $rc -ne 0 ]]; then
             echo "Error: Failed to query AWS." | tee -a "$log"
@@ -73,15 +87,15 @@ for name in $names; do
     fi
     echo "instance_id: $instance_id" | tee -a "$log"
 
-    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
+    aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
     rc=$?
     if [[ $rc -ne 0 ]]; then
         echo "Error: Failed to query AWS." | tee -a "$log"
         exit 1
     fi
-    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
+    aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
 
-    aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
+    aws --profile "$profile" $region_opt ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
 
     if [[ ! $do_not_prompt ]]; then
         echo -n 'Are you sure that you want this instance started? y/n: '
@@ -94,10 +108,10 @@ for name in $names; do
     fi
 
     echo "Start instance..." | tee -a "$log"
-    aws --profile "$profile" ec2 start-instances --instance-ids "$instance_id" --output table | tee -a "$log"
+    aws --profile "$profile" $region_opt ec2 start-instances --instance-ids "$instance_id" --output table | tee -a "$log"
     state=""
     while [[ $state != "running" ]] && [[ $wait_for_running == yes ]]; do
-        state=$(aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[State.Name]' --output text)
+        state=$(aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[State.Name]' --output text)
         echo -n "."
         sleep 1
     done
--- aws/ec2/ec2_stop_instance.sh
+++ aws/ec2/ec2_stop_instance.sh
@@ -5,20 +5,24 @@
 # Usage:
 #     Run script with --help option to get usage.
 
-version="1.1.1"
+version="1.2.0"
 
 set -o pipefail
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 script_name="$(basename "$0")"
 log="$script_dir/${script_name/.sh/.log}"
 
+profile="${AWS_PROFILE:-default}"
+
 usage() {
     echo "Usage:"
     echo "    export AWS_PROFILE=profile"
     echo
-    echo "    $script_name [-f] [-n] [-w] {name | instance_id}..."
+    echo "    $script_name [--profile profile] [--region region] [-f] [-n] [-w] {name | instance_id}..."
     echo
     echo "Description:"
+    echo "    --profile          Use a specified profile from your AWS credential file, otherwise get it from AWS_PROFILE variable."
+    echo "    --region           Use a specified region instead of region from configuration or environment setting."
     echo "    -f, --force        Force stop instance."
     echo "    -n, --no-prompt    No confirmation prompt."
     echo "    -w, --wait         Wait for 'stopped' state before finishing."
@@ -28,6 +32,17 @@ usage() {
 
 while test -n "$1"; do
     case "$1" in
+    --profile)
+        shift
+        profile="$1"
+        shift
+        ;;
+    --region)
+        shift
+        region="$1"
+        region_opt="--region=$region"
+        shift
+        ;;
     -h|--help)
         usage
         ;;
@@ -52,7 +67,6 @@ while test -n "$1"; do
         shift
     esac
 done
-profile="${AWS_PROFILE:-default}"
 
 if [[ -z $names ]]; then
     usage
@@ -69,7 +83,7 @@ for name in $names; do
         instance_id="$name"
     else
         echo "name: $name" | tee -a "$log"
-        instance_id=$(aws --profile "$profile" ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
+        instance_id=$(aws --profile "$profile" $region_opt ec2 describe-instances --filters "Name=tag:Name, Values=$name" --query 'Reservations[].Instances[].[InstanceId]' --output text)
         rc=$?
         if [[ $rc -ne 0 ]]; then
             echo "Error: Failed to query AWS." | tee -a "$log"
@@ -78,15 +92,15 @@ for name in $names; do
     fi
     echo "instance_id: $instance_id" | tee -a "$log"
 
-    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
+    aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[Tags[?Key==`Name`].Value | [0], InstanceId, Placement.AvailabilityZone, InstanceType, State.Name]' --output table
     rc=$?
     if [[ $rc -ne 0 ]]; then
         echo "Error: Failed to query AWS." | tee -a "$log"
         exit 1
     fi
-    aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
+    aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --output table >> "$log"
 
-    aws --profile "$profile" ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
+    aws --profile "$profile" $region_opt ec2 describe-volumes --filters "Name=attachment.instance-id, Values=$instance_id" --query 'Volumes[*].{VolumeId:VolumeId,InstanceId:Attachments[0].InstanceId,State:Attachments[0].State,DeleteOnTermination:Attachments[0].DeleteOnTermination,Device:Attachments[0].Device,Size:Size}' --output table | tee -a "$log"
 
     if [[ ! $do_not_prompt ]]; then
         echo -n 'Are you sure that you want this instance stopped? y/n: '
@@ -106,7 +120,7 @@ for name in $names; do
     fi
     state=""
     while [[ $state != "stopped" ]] && [[ $wait_for_stopped == yes ]]; do
-        state=$(aws --profile "$profile" ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[State.Name]' --output text)
+        state=$(aws --profile "$profile" $region_opt ec2 describe-instances --instance-ids "$instance_id" --query 'Reservations[].Instances[].[State.Name]' --output text)
         echo -n "."
         sleep 1
     done
